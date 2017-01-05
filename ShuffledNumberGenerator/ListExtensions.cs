d35e133b33f754149e65597be685cb863a12cf3d
Jamie Bertram <jamie@novoideation.ca>
2017-01-05 13:47:41 -0700

    Added doc comments to ListExtenstions module

--- ShuffledNumberGenerator/ListExtensions.cs
+++ ShuffledNumberGenerator/ListExtensions.cs
@@ -8,8 +8,19 @@ namespace ShuffledNumberGenerator
 {
     public static class ListExtensions
     {
+        /// <summary>
+        /// Swaps two elements within the given list.  Swap is performed
+        /// in-place (mutates the list).
+        /// </summary>
+        /// <typeparam name="T">The type of element the list contains</typeparam>
+        /// <param name="list">The "this" argument of this extension method</param>
+        /// <param name="firstIndex">The index of the first item to swap</param>
+        /// <param name="secondIndex">The index of the second item to swap</param>
+        /// <exception cref="ArgumentOutOfRangeException">If either the first or second index are
+        /// negative, or greater than or equal to the number of elements in the list</exception>
         public static void SwapElements<T>(this List<T> list,  int firstIndex, int secondIndex)
         {
+            // Check for invalid arguments
             if (firstIndex < 0 || firstIndex >= list.Count)
                 throw new ArgumentOutOfRangeException(nameof(firstIndex),
                     "cannot swap element at index " + firstIndex + " in list of size " + list.Count);
@@ -18,9 +29,11 @@ namespace ShuffledNumberGenerator
                 throw new ArgumentOutOfRangeException(nameof(secondIndex),
                     "cannot swap element at index " + secondIndex + " in list of size " + list.Count);
 
+            // Return early if indecies are the same
             if (firstIndex == secondIndex)
-                return; // Return early, meaningless to swap an element with its self
+                return; 
 
+            // Swap the elements
             var temp = list[firstIndex];
             list[firstIndex] = list[secondIndex];
             list[secondIndex] = temp;
